1b0d417932431457ee9adddbf5d6cfc61b1feecd
0ya-sh0 <ymthatte@gmail.com>
2021-02-25 16:47:54 +0530

    refactor

--- src/server.js
+++ src/server.js
@@ -1,10 +1,45 @@
-const app = require('./app');
-const http = require('http').Server(app);
+const express = require('express');
+const expressServer = express();
+
+expressServer.get('/', function (req, res) {
+	res.redirect('/home');
+});
+expressServer.get('/home', function (req, res) {
+	res.sendFile(__dirname + '/public/views/home.html');
+});
+expressServer.get('/game', function (req, res) {
+	res.sendFile(__dirname + '/public/views/game.html');
+});
+expressServer.use('/styles', express.static(__dirname + '/public/styles'));
+expressServer.use('/scripts', express.static(__dirname + '/public/scripts'));
+
+const http = require('http');
+const httpServer = http.Server(expressServer);
 const { port } = require('./config.json');
-const initSocket = require('./io');
+const socketio = require('socket.io');
 
-initSocket(http);
+const io = socketio(httpServer);
+io.on('connection', connectionHandler);
 
-http.listen(port, () => {
+function connectionHandler(socket) {
+	console.log('user connected ...');
+
+	const CreateGame = require('./events/createGame');
+	socket.on(CreateGame.name, (id) => CreateGame.handler(socket, id));
+
+	const JoinGame = require('./events/joinGame');
+	socket.on(JoinGame.name, (id) => JoinGame.handler(socket, id));
+
+	const Move = require('./events/move');
+	socket.on(Move.name, (id, move) => Move.handler(socket, id, move));
+
+	const EndGame = require('./events/endGame');
+	socket.on(EndGame.name, (id) => EndGame.handler(socket, id));
+
+	const Disconnect = require('./events/disconnect');
+	socket.on(Disconnect.name, (id) => Disconnect.handler(socket));
+}
+
+httpServer.listen(port, () => {
 	console.log('listening...');
 });
